7e77f604c69e9dec0a825e1252af7f3dd4280939
vitormnegreiros <145074220+vitormnegreiros@users.noreply.github.com>
2024-11-19 23:16:37 -0300

    Update cadastroTecnico.js

--- crud-app/routes/cadastroTecnico.js
+++ crud-app/routes/cadastroTecnico.js
@@ -1,9 +1,16 @@
+require('dotenv').config();
+
 const express = require('express');
+const bcrypt = require('bcrypt');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 const db = require('../db/dbConnection');
+const authenticateToken = require('../middleware/authMiddleware');
+
 
 // Criar novo técnico
-router.post('/', (req, res) => {
+// Adicionando o middleware de autenticação para proteger a rota
+router.post('/', authenticateToken, (req, res) => {
     const { nome, cpf, telefone } = req.body;
     const query = 'INSERT INTO cadastroTecnico (nome, cpf, telefone) VALUES (?, ?, ?)';
     
@@ -15,50 +22,5 @@ router.post('/', (req, res) => {
         res.status(201).send('Técnico cadastrado com sucesso');
     });
 });
-
-
-// Ler todos os técnicos cadastrados
-/*router.get('/', (req, res) => {
-    db.query('SELECT * FROM cadastroTecnico', (err, results) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        res.status(200).json(results);
-    });
-});*/
-
-// Ler um técnico pelo CPF (usando req.body)
-/*router.get('/cpf', (req, res) => {
-    const { cpf } = req.body;
-    db.query('SELECT * FROM cadastroTecnico WHERE cpf = ?', [cpf], (err, result) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        res.status(200).json(result[0]);
-    });
-});*/
-
-// Atualizar um técnico (usando req.body)
-/*router.put('/cpf', (req, res) => {
-    const { cpf, nome, telefone } = req.body;
-    const query = `UPDATE cadastroTecnico SET nome = ?, telefone = ? WHERE cpf = ?`;
-    db.query(query, [nome, telefone, cpf], (err, result) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        res.status(200).send('Dados do técnico atualizados com sucesso');
-    });
-});*/
-
-// Deletar um técnico (usando req.body)
-/*router.delete('/cpf', (req, res) => {
-    const { cpf } = req.body;
-    db.query('DELETE FROM cadastroTecnico WHERE cpf = ?', [cpf], (err, result) => {
-        if (err) {
-            return res.status(500).send(err);
-        }
-        res.status(200).send('Técnico deletado com sucesso');
-    });
-});*/
 
 module.exports = router;
