b2bcf34a25fde4afe339202ae501099f93955f78
vitormnegreiros <145074220+vitormnegreiros@users.noreply.github.com>
2024-11-19 23:14:00 -0300

    Update authMiddleware.js

--- crud-app/middleware/authMiddleware.js
+++ crud-app/middleware/authMiddleware.js
@@ -1,1 +1,25 @@
+// authMiddleware.js
+const jwt = require('jsonwebtoken');
+const SECRET_KEY = process.env.SECRET_KEY;
+
+function authenticateToken(req, res, next) {
+    const token = req.header('Authorization')?.replace('Bearer ', '');
+
+    console.log(token)
+
+    if (!token) {
+        return res.status(401).send({ error: 'Acesso não autorizado' });
+    }
+
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+        if (err) {
+            return res.status(403).send({ error: 'Token inválido' });
+        }
+
+        req.user = user;
+        next();
+    });
+}
+
+module.exports = authenticateToken;
 
