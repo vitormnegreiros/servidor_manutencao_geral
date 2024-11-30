8fe02487975664a8bc1febc54e53b000c978adcd
vitormnegreiros <145074220+vitormnegreiros@users.noreply.github.com>
2024-11-30 18:16:03 -0300

    Update dbConnection.js

--- crud-app/db/dbConnection.js
+++ crud-app/db/dbConnection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2');
 
+
 // Cria a conexão com o banco de dados
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'manutencaoGeral'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 
 // Conecta ao banco
